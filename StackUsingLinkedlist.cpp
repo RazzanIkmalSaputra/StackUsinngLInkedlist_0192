1a774b2fb14365cddf125da1f4f25be909b044c2
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2025-05-22 09:59:34 +0700

    menambahkan class node

--- StackUsingLinkedlist.cpp
+++ StackUsingLinkedlist.cpp
@@ -2,3 +2,14 @@
 
 using namespace std;
 
+//node class representing a single node in the linked list
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node() {
+        next = NULL;
+    }
+};
+
